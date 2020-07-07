49e4ada0813798744bbadf442ac438905c491d04
Kiril <kiril.bogdan.97@gmail.com>
2020-07-08 00:29:22 +0300

    Initial Commit

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,18 @@
 $(document).ready(function () {
-  console.log('Hello Wordfsdfsdfdld')
-  console.log('Clyde Shelton 3');
+  $('.header__slider').slick({
+    arrows: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    prevArrow: '<div class="arrow-left"></div>',
+    nextArrow: '<div class="arrow-right"></div>',
+    appendDots: $('.header__content'),
+    infinite: false
+  });
+
+  $('.header__burger').click(function (event) {
+    $('.header__burger,.header__menu').toggleClass('active');
+    $('body').toggleClass('lock');
+    // Чтобы не скролилась страница под бургером меню
+  });
 })
