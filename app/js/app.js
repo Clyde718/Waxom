9304a44ca2815d2f32d77eb8773a90b388b98274
Kiril <kiril.bogdan.97@gmail.com>
2020-07-10 20:21:04 +0300

    Initia; Commit

--- app/js/app.js
+++ app/js/app.js
@@ -71,5 +71,42 @@ $(document).ready(function () {
 
 
 
+  // number animation
+  function countup(className) {
+    var countBlockTop = $("." + className).offset().top;
+    var windowHeight = window.innerHeight;
+    var show = true;
+
+    $(window).scroll(function () {
+      if (show && (countBlockTop < $(window).scrollTop() + windowHeight)) {
+        show = false;
+
+        $('.' + className).spincrement({
+          from: 1,
+          duration: 4000,
+        });
+      }
+    })
+  }
+
+
+
+
+
+  $(function () {
+    countup("facts__number", $(".facts__number").text());
+  });
+
+
+
+
+
+
+
+
+
 });
+
+
+
 
